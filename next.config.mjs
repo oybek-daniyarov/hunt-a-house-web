84cdecf92c1ba0e460841b9cfdf1ad339736dc49
oybek <digital@oybek.dev>
2025-01-15 03:53:43 +0500

    feat(core): added property pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "cdn.sanity.io",
+            },
+        ],
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    }
 };
 
 export default nextConfig;
