69631014d472c55dbc55f6a903f99d3ae54b65c7
oybek <digital@oybek.dev>
2025-02-08 04:15:26 +0500

    fix

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 
 import DesktopNav from '@/components/header/desktop-nav';
 import MobileNav from '@/components/header/mobile-nav';
+import Logo from '@/components/logo';
 import { ModeToggle } from '@/components/menu-toggle';
 import AuthButton from './auth-button';
 
@@ -11,26 +12,26 @@ const navItems = [
     href: '/',
     target: false,
   },
-  // {
-  //   label: 'For Agents',
-  //   href: '/agent',
-  //   target: false,
-  // },
-  // {
-  //   label: 'For Clients',
-  //   href: '/client',
-  //   target: false,
-  // },
-  // {
-  //   label: 'For Clients V2',
-  //   href: '/clients-v2',
-  //   target: false,
-  // },
-  // {
-  //   label: 'Property Listings',
-  //   href: '/property-listings',
-  //   target: false,
-  // },
+  {
+    label: 'For Agents',
+    href: '/agent',
+    target: false,
+  },
+  {
+    label: 'For Clients',
+    href: '/client',
+    target: false,
+  },
+  {
+    label: 'For Clients V2',
+    href: '/clients-v2',
+    target: false,
+  },
+  {
+    label: 'Property Listings',
+    href: '/property-listings',
+    target: false,
+  },
   {
     label: 'About',
     href: '/about',
@@ -43,7 +44,7 @@ export default function Header() {
     <header className="sticky top-0 w-full border-border/40 bg-background/95 z-50">
       <div className="container flex items-center justify-between h-14">
         <Link href="/" aria-label="Home page">
-          {/* <Logo /> */}
+          <Logo />
         </Link>
         <div className="hidden xl:flex gap-7 items-center justify-between">
           <DesktopNav navItems={navItems} />
