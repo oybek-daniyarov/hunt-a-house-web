74c6d115276267464b5371c4959e6b3fa0a4b5dd
Oybek <oybek@karvedigital.com>
2025-03-02 20:15:54 +0500

    refactor: update cookie options for enhanced security and domain handling

--- lib/client/laravel/contants.ts
+++ lib/client/laravel/contants.ts
@@ -2,4 +2,7 @@ import { ResponseCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 
 export const COOKIE_OPTIONS: Partial<ResponseCookie> = {
   expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
+  sameSite: 'none',
+  secure: true,
+  domain: process.env.DOMAIN || 'localhost',
 };
