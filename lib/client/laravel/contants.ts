7b53a6d2a8e3fe2f6e4b1f0d02aeea974a18d131
Oybek <oybek@karvedigital.com>
2025-03-02 19:46:53 +0500

    fix: simplify secure cookie option for production environment

--- lib/client/laravel/contants.ts
+++ lib/client/laravel/contants.ts
@@ -2,7 +2,7 @@ import { ResponseCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 
 export const COOKIE_OPTIONS: Partial<ResponseCookie> = {
   httpOnly: true, // Secure against XSS
-  secure: process.env.NODE_ENV === 'production' ? true : false,
+  secure: process.env.NODE_ENV === 'production',
   sameSite: 'lax',
   path: '/',
   maxAge: 30 * 24 * 60 * 60, // 30 days
